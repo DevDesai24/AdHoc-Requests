03ae3e1683cbdf9cd5d394cdaa40912078fcd2ba
Dev Desai <dev24desai@gmail.com>
2025-05-24 14:59:51 +0530

    ADhoc5.sql

--- ADhoc5.sql
+++ ADhoc5.sql
@@ -1,21 +1,3 @@
-select 
-	product, 
-    p.product_code, 
-    manufacturing_cost
-from dim_product p
-join fact_manufacturing_cost m
-on p.product_code=m.product_code
-where manufacturing_cost=(select max(manufacturing_cost) from fact_manufacturing_cost)
-union all
-select 
-	product, 
-    p.product_code, 
-    manufacturing_cost
-from dim_product p
-join fact_manufacturing_cost m
-on p.product_code=m.product_code
-where manufacturing_cost=(select min(manufacturing_cost) from fact_manufacturing_cost);
-
 SELECT 
 	product, 
     p.product_code, 
